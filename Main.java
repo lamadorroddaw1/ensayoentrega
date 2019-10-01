4869251a3c7ef216aaa38b4067d476c232ef620d
Lander <lamadorroddaw1@gmail.com>
2019-10-01 12:11:28 +0200

    Eliminada println del main

--- Main.java
+++ Main.java
@@ -14,7 +14,6 @@ public class Main
      */
     public static void main(String[] args){
          System.out.println("Probando BlueJ");
-         System.out.println("xxxxx");
          System.out.println("Modificado desde casa");
     }
 }
